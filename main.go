2aba9df9f281a0853b0950aa1fd6ae9c7de7eea1
Pablo Uribe <puribe@soaint.com>
2023-09-20 12:25:37 -0500

    siguiente commit

--- main.go
+++ main.go
@@ -1,10 +1,16 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/pablouribe1987/Gopractice/variables"
 )
 
 func main() {
 	//fmt.Println("Hola Mundo")
-	variables.MuestroEnteros()
+	//variables.MuestroEnteros()
+	//variables.RestoVariables()
+	estado, texto := variables.ConviertoaTexto(1588)
+	fmt.Println(estado)
+	fmt.Println(texto)
 }
